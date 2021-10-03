cbab208eaef829e48b7ab7da054340d2ab48271a
Zi-long QIU <zilong.qiu@gmail.com>
2021-10-03 17:41:40 +0800

    [main] feat: enable icmp protocol on AWS Lightsail

--- templates/compute/aws_lightsail/modules/compute/compute.tf
+++ templates/compute/aws_lightsail/modules/compute/compute.tf
@@ -2,7 +2,7 @@
 resource "aws_lightsail_instance" "test" {
   name              = "instance-test"
   availability_zone = "ap-northeast-1a"
-  blueprint_id      = "amazon_linux_2"
+  blueprint_id      = "ubuntu_20_04"
   bundle_id         = "nano_2_0"
   # key_pair_name     = "my_key_pair"
   # user_data         = "${file("userdata.sh")}" 
@@ -16,5 +16,27 @@ resource "aws_lightsail_instance" "test" {
     # private_key = "${file("my_key.pem")}"
     user        = "user"
     timeout     = "20s"
+  }
+}
+
+resource "aws_lightsail_instance_public_ports" "test" {
+  instance_name = aws_lightsail_instance.test.name
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 80
+    to_port   = 80
+  }
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+  }
+
+  port_info {
+    protocol  = "icmp"
+    from_port = 8
+    to_port   = 0
   }
 }
